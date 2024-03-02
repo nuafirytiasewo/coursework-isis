62e9842e80995d936f26c39826f3277564e33c11
Dmitry <103138302+nuafirytiasewo@users.noreply.github.com>
2024-03-02 12:35:33 +0400

    добавление контроллеров и представлений к таблицам из бд

---
+++ coursework/Controllers/ServicesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using coursework.Models;
+
+namespace coursework.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private ADOModelDB db = new ADOModelDB();
+
+        // GET: Services
+        public ActionResult Index()
+        {
+            var services = db.Services.Include(s => s.ServiceCategories);
+            return View(services.ToList());
+        }
+
+        // GET: Services/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Services services = db.Services.Find(id);
+            if (services == null)
+            {
+                return HttpNotFound();
+            }
+            return View(services);
+        }
+
+        // GET: Services/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.ServiceCategories, "CategoryID", "Name");
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ServiceID,Name,Description,Price,CategoryID")] Services services)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Services.Add(services);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.ServiceCategories, "CategoryID", "Name", services.CategoryID);
+            return View(services);
+        }
+
+        // GET: Services/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Services services = db.Services.Find(id);
+            if (services == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.ServiceCategories, "CategoryID", "Name", services.CategoryID);
+            return View(services);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ServiceID,Name,Description,Price,CategoryID")] Services services)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(services).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.ServiceCategories, "CategoryID", "Name", services.CategoryID);
+            return View(services);
+        }
+
+        // GET: Services/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Services services = db.Services.Find(id);
+            if (services == null)
+            {
+                return HttpNotFound();
+            }
+            return View(services);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Services services = db.Services.Find(id);
+            db.Services.Remove(services);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
