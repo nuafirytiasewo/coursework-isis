c665fde04e82a807e51ffec85a60f57159652c33
Dima M <devontelnel@mail.ru>
2024-03-04 17:21:37 +0400

    2 выборка

--- coursework/Controllers/SelectionsController.cs
+++ coursework/Controllers/SelectionsController.cs
@@ -37,5 +37,27 @@ namespace coursework.Controllers
 
             return View(top5ClientsByRequests);
         }
+
+        public ActionResult Index2()
+        {
+            // Проверяем, аутентифицирован ли пользователь
+            if (!AuthenticationHelper.CheckAuthentication(Session, ViewBag, false))
+            {
+                return RedirectToAction("Login", "MyAccount");
+            }
+            // 2 выборка: Выбор информации о сотруднике с наивысшей зарплатой
+            var highestSalary = db.Employees.Max(e => e.Salary);
+            var employee = db.Employees.FirstOrDefault(e => e.Salary == highestSalary);
+            var highestPaidEmployee = employee != null ? new[] { new ModelForSelection_2
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Patronymic = employee.Patronymic,
+                Salary = employee.Salary
+            } } : new ModelForSelection_2[0];
+
+            return View(highestPaidEmployee);
+
+        }
     }
 }
---
+++ coursework/Models/ModelForSelection_2.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace coursework.Models
+{
+    public class ModelForSelection_2
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public decimal? Salary { get; set; }
+
+    }
+}
