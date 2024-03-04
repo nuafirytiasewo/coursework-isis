a92230742a0ebb75c9009713122ecbc3d743cd33
Dima M <devontelnel@mail.ru>
2024-03-04 12:58:42 +0400

    Добавлена первая выборка

---
+++ coursework/Controllers/SelectionsController.cs
@@ -0,0 +1,41 @@
+﻿using coursework.Controllers.Helpers;
+using coursework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace coursework.Controllers
+{
+    public class SelectionsController : Controller
+    {
+        private readonly ADOModelDB db = new ADOModelDB();
+        // GET: Selections
+        public ActionResult Index1()
+        {
+            // Проверяем, аутентифицирован ли пользователь
+            if (!AuthenticationHelper.CheckAuthentication(Session, ViewBag, false))
+            {
+                return RedirectToAction("Login", "MyAccount");
+            }
+            // 1 выборка: Получение топ-5 клиентов, которые оставили наибольшее количество заявок
+            var top5ClientsByRequests = db.Requests
+                .GroupBy(r => r.ClientID)
+                .OrderByDescending(g => g.Count())
+                .Take(5)
+                .Join(db.Clients,
+                      r => r.Key, // ClientID из группировки
+                      c => c.ClientID, // ClientID из коллекции клиентов
+                      (group, client) => new ModelForSelection_1 { 
+                          LastName = client.LastName, 
+                          FirstName = client.FirstName, 
+                          Patronymic = client.Patronymic, 
+                          Count = group.Count() 
+                      })
+                .ToList();
+
+            return View(top5ClientsByRequests);
+        }
+    }
+}
---
+++ coursework/Models/ModelForSelection_1.cs
@@ -0,0 +1,11 @@
+﻿namespace coursework.Models
+{
+    public class ModelForSelection_1
+    {
+        public string LastName { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string Patronymic { get; internal set; }
+        public int Count { get; set; }
+       
+    }
+}
